17a1ed4b1972cff24f460c7eaa3205b11458c138
ankitoscar <ankitoscar911@gmail.com>
2021-03-23 23:01:38 +0530

    added app in installed apps

--- travel/settings.py
+++ travel/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blog.apps.BlogConfig'
 ]
 
 MIDDLEWARE = [
@@ -105,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Kolkata'
 
 USE_I18N = True
 
@@ -119,4 +120,4 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (str(BASE_DIR.joinpath('static')))
+STATICFILES_DIRS = [str(BASE_DIR.joinpath('static'))]

baa3b76a1ff81658d9d2fd963fb6a68c693a34e1
ankitoscar <ankitoscar911@gmail.com>
2021-03-25 20:46:57 +0530

    added page-2 url

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views 
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('page-2/', views.page_2, name='page-2')
 ]

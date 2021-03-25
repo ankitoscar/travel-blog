26504760690360eea2be656a32de1cf9f417c27e
ankitoscar <ankitoscar911@gmail.com>
2021-03-25 20:47:20 +0530

    added page-2 view

--- blog/views.py
+++ blog/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'blog/index.html')
+
+def page_2(request):
+    return render(request, 'blog/page-2.html')

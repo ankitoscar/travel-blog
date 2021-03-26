7c7a54d8bf966513e3cacc526a402d205ae521c8
ankitoscar <ankitoscar911@gmail.com>
2021-03-26 23:39:55 +0530

    added blogger model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django_countries.fields import CountryField
+import uuid
 
 # Create your models here.
+class Blogger(models.Model):
+    user_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=30)
+    email = models.CharField(max_length=30)
+    country = CountryField()
+    about = models.CharField(max_length=140, blank='Write something about yourself in 140 characters.')

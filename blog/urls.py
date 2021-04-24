dee09830c5c99504471fc45797e50d67f25ffa67
ankitoscar <ankitoscar911@gmail.com>
2021-04-24 15:07:02 +0530

    removed signup and signin views

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('page-2/', views.page_2, name='page-2'),
-    path('page-3/', views.page_3, name='page-3')
+    path('page-3/', views.page_3, name='page-3'),
 ]

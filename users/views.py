2513791cd3071900c8ef67fb8f5bff2aade5a9b7
ankitoscar <ankitoscar911@gmail.com>
2021-04-27 16:52:05 +0530

    added login form and view

--- users/views.py
+++ users/views.py
@@ -1,11 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login 
+from django.urls import reverse
+from users.forms import CustomUserCreationForm
 
 # Create your views here.
 def signup(request):
     return render(request, 'users/signup.html')
 
 def signin(request):
-    return render(request, 'users/signin.html')
+    if request.method == 'GET':
+        return render(request, 'registration/login.html', {"form": CustomUserCreationForm})
+    elif request.method =='POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect(reverse("index"))
 
 def logout(request):
     return render(request, 'users/logout.html')
